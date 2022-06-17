60b5675275f0e263976303acdc7ba37cc1c9bf66
GustavFluur <gustavfluur@hotmail.com>
2022-06-17 13:04:00 +0000

    Added more content and learnings into the js file

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -4,7 +4,24 @@
  */
 
 // Regular function call 
+const sumAll = (a, b, c) => a + b + c;
+let sum = sumAll(1, 2, 3)
+console.log("Sum:", sum);
 
 // Extra arguments are ignored
+let sum2 = sumAll(1, 2, 3, 4, 5, 6);
+console.log("Sum2", sum2);
+// comment: Here all numbers will be shown in the terminal, which we don't want... What do we need to do for making the last numbers disappear? 
 
 // Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    } 
+    return sum;
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log("Sum3", sum3);
+
